be8f0a37de37dd9f416fbfec480cf9ae4bd8f615
Zakariya khan <73132031+Ardent10@users.noreply.github.com>
2021-09-17 19:41:14 +0530

    Update game.js

--- game.js
+++ game.js
@@ -59,7 +59,7 @@ function nextSequence(){
 // function for play sound
 function playSound(name){
 
-  var audio = new Audio("sounds/"+name+".mp3");
+  var audio = new Audio(name+".mp3");
   audio.play();
 }
 
